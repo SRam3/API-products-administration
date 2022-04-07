f0d6025487e3df0afe26f6ec97eb0de9e04dd2ca
Sebastian Ramirez <megatron@Sebastians-MacBook-Pro.local>
2022-04-07 11:19:15 -0500

    second commint

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+#Objeto que vamos a configurar
+app = Flask(__name__)
+
+#Cada vez que un usuario entre a la ruta principal se le va a responder algo
+@app.route('/')
+def hello_world():
+    return 'Hello, Sebastián!'
+
+#rutas    
+@app.route('/add_contact')
+def add_contact():
+    return 'add contact'
+
+@app.route('/edit')
+def edit_contact():
+    return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+    return 'delete contact'
+
+
+#Validacion del archivo principal
+if __name__ == '__main__':
+    app.run(debug=True) #Cada vez que hago cambios se reinicie automaticamente
