6691db6cd2f94516f8db1eb7e02a42d5cd6aa6f7
Sebastian Ramirez <megatron@Sebastians-MacBook-Pro.local>
2022-04-19 15:48:45 -0500

    modifications

--- products.py
+++ products.py
@@ -1,6 +1,9 @@
+from math import prod
+import uuid
+
 products = [ 
-    {"name": "pencil", "price": 5, "quantity": 100},
-    {"name": "notebook", "price": 30, "quantity": 20},
-    {"name": "eraser", "price": 3, "quantity": 200},
-    {"name": "book", "price": 40, "quantity": 30}
+    {"id":str(uuid.uuid4()),"name": "pencil", "price": 5, "quantity": 100},
+    {"id":str(uuid.uuid4()), "name": "notebook", "price": 30, "quantity": 20},
+    {"id":str(uuid.uuid4()), "name": "eraser", "price": 3, "quantity": 200},
+    {"id":str(uuid.uuid4()), "name": "book", "price": 40, "quantity": 30}
 ]
